913843657597876b885b45cc8f185d4aae0966e7
OmarFarooqui <MohammedFarooqui@LAPTOP-9029EBUH>
2025-02-17 13:55:27 +0000

    initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(PORT, () => console.log("Server is running on port: " + PORT));
+
