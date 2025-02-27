6b270595212014a6c717e2b4ee93ae83c114f955
OmarFarooqui <MohammedFarooqui@LAPTOP-9029EBUH>
2025-02-27 16:12:10 +0000

    added frontend ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<Dashboard/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<Register/>} />
-        </Routes>
+        <div className="Container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FaSignOutAlt, FaSignInAlt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  const user = null;
+  const onLogout = () => {};
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">GoalSetter</Link>
+      </div>
+      <ul>
+        {user ? (
+          <li>
+            <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
